aac9996f8402e467f74b0aff219e62265d9f882c
Sergey <sergnm33@gmail.com>
2022-04-04 01:23:49 +0700

    вывод номера словаря

--- Продвинутые - 10 Словари/10-2---02.py
+++ Продвинутые - 10 Словари/10-2---02.py
@@ -18,3 +18,6 @@ users = [{'name': 'Todd', 'phone': '551-1414', 'email': 'todd@gmail.com'},
          {'name': 'Maria', 'phone': '12-129-3148', 'email': 'm.sharapova@gmail.com'},
          {'name': 'Fedor', 'phone': '+7445-341-0545', 'email': ''},
          {'name': 'Tim', 'phone': '242-449-3141', 'email': 'timm.ggg@yandex.ru'}]
+
+for i in range(len(users)):
+    phone=users[i]['phone']
